c89840845bc81d19de66b5f89f7db92bde20d8e7
Oluwadamilare EZEKIEL <oluwadamilare.ezekiel@kibo.school>
2024-11-10 18:54:38 +0100

    Feat: Jsonify recommendation analysis

--- app/services/analyzer.py
+++ app/services/analyzer.py
@@ -3,6 +3,8 @@ import os
 from openai import OpenAI
 from dto import CampaignData
 from IPython.display import Markdown
+import json
+
 
 client = OpenAI(api_key=os.environ.get("OPENAI_API_KEY"))    
 
@@ -10,6 +12,7 @@ def get_function_response(data: CampaignData, function_object, tool_choice = "re
   prompt = f"""
     Analyze the following crowdfunding campaign details and provide a score out of 100. 
     Additionally, suggest areas of improvement with headings and subheadings:
+    Note: Only provide recommendations based on these information only.
 
     Campaign Name: {data.name}
     Description: {data.description}
@@ -30,6 +33,15 @@ def get_function_response(data: CampaignData, function_object, tool_choice = "re
     else:
         result = response.choices[0].message.content
 
-    return result
+    try:
+      result_json = json.loads(result)
+    except json.JSONDecodeError:
+        # If it's not already JSON, structure it manually
+      result_json = {
+                "score": None,
+                "improvements": result
+            }
+
+    return result_json  
   except Exception as e:
       return f"Error occurred while analyzing: {e}"
